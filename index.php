3f478178f0fcd04c9784fede57bf5bb514f7447e
=Bittersei <=sei.hdzb@gmail.com>
2022-06-24 00:04:34 -0500

    Carga de la libreria AOS

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <html>
-<head>
-	<title>Bittersei</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="css/home-dark.css"/>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-</head>
-<body>
-	<div id="navi" class="animate__animated animate__fadeInRight"  style="width:100%; height: 5vh; background-color:slateblue; overflow-x: hidden;">
-		<a>Home</a> <a>About</a> <a>Working</a>
-	</div>
-	<div id="header" style="height: 60vh; border:solid azure 3px; overflow-x: hidden; background-color:white;">
-
-	</div>
-	<div id="about" style="height: 60vh; border:solid azure 3px; overflow-x: hidden;">
+	<head>
+		<title>Bittersei</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="css/home-dark.css"/>
 		
-	</div>
-</body>
+		
+	</head>
+	<body>
+		<div id="navi"  style="width:100%; height: 5vh; background-color:slateblue; overflow-x: hidden;">
+			<a>Home</a> <a>About</a> <a>Working</a>
+		</div>
+		<div id="header"  style="height: 60vh; border:solid azure 3px; overflow-x: hidden; background-color:black;">
+
+		</div>
+		<div id="about"   style="height: 60vh; border:solid azure 3px; overflow-x: hidden;">
+			
+		</div>
+		<div id="working"   style="height: 60vh; border:solid azure 3px; overflow-x: hidden;">
+			
+		</div>
+		<div id="footer"  style="height: 60vh; border:solid azure 3px; overflow-x: hidden;">
+			
+		</div>
+	</body>
 </html>
